9aeea627f536691541e7dd14bd864bec81f20064
Peter Stein <peterstein@protonmail.com>
2018-10-24 12:26:21 +0200

    Create lumis view

--- lumis/settings.py
+++ lumis/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lumisapp.apps.LumisappConfig',
+    'bootstrap3',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
